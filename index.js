db46539f1bc406089f34dd3732ec86b9072ac208
Antonyrahul <antonyrahul96@gmail.com>
2020-06-10 23:30:23 +0530

    Push to mongo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongodbclient = require('mongodb');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const jwt =  require("jsonwebtoken");
-url = /*"mongodb+srv://antonyrahul96:antonyrahul96@cluster0-nl7jd.mongodb.net/test?retryWrites=true&w=majority"*/"mongodb://localhost:27017/"
+url = "mongodb+srv://antonyrahul96:antonyrahul96@cluster0-ficsj.mongodb.net/test?retryWrites=true&w=majority"/*"mongodb://localhost:27017/"*/
 productdburl = "mongodb://localhost:27017/productDB";
 usersdburl = "mongodb://localhost:27017/userDB";
 studentsLoginDB = "studentsDB"
@@ -42,36 +42,7 @@ app.post('/generateurl', function (req, res) {
 app.get('/', function (req, res) {
   
    console.log("Connection from angular")
-   //mongodb+srv://antonyrahul96:<password>@cluster0-jjxyl.mongodb.net/test?retryWrites=true&w=majority
-//mongodb+srv://antonyrahul96:<password>@cluster0-nl7jd.mongodb.net/test?retryWrites=true&w=majority
-   //"mongodb://localhost:27017/productDB"
-   //mongodb+srv://antonyrahul96:<password>@cluster0-nl7jd.mongodb.net/test?retryWrites=true&w=majority
-/*mongodbclient.connect(url, function (err, client) {
-    if (err) throw err;
-    var db = client.db("productDB");
-    
-        
-        var userData = {
-            id:"mobile2",
-            name:"Iphone 10",
-            category:"mobiles",
-            price : "INR 110,000",
-            imgsrc : "https://www.appsquadz.com/blog/wp-content/uploads/2016/12/iPhone-Apps.png",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
-            
-        }
-        db.collection("products").insertOne(userData, function (err, data) {
-            if (err) throw err;
-            client.close();
-            res.json({
-                message: "saved"
-            })
-        })
-        // Store hash in your password DB.
-  
 
-   // client.close();
-});*/
 })
 app.get('/generatetestid',function(req,res)
 {
@@ -161,7 +132,7 @@ app.post('/loginuser', function (req, res) {
                 }
                     else{
                         client.close();
-                        res.status(401).json({
+                        res.json({
                             message: "Incorrect password"
                         })
                     
@@ -175,7 +146,7 @@ app.post('/loginuser', function (req, res) {
             else
             {
                 client.close();
-                res.status(401).json({
+                res.json({
                     message : "Incorrect username"
                 })
             }
@@ -311,7 +282,7 @@ app.post('/getavailabletests', function (req, res) {
             
                 
            
-            db.collection(questionCollection).find({}).toArray(function (err, data) {
+            db.collection(questionCollection).find(req.body).toArray(function (err, data) {
                 console.log(data)
                 if (err) throw err;
                 client.close();
@@ -333,6 +304,75 @@ app.post('/getavailabletests', function (req, res) {
     });
 
 })
+
+app.post('/gettakentests', function (req, res) {
+    console.log(req.body);
+    mongodbclient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db(answersheetsDB);
+      
+           
+            
+                
+           
+            db.collection(answersheetscollection).find(req.body).toArray(function (err, data) {
+                console.log(data)
+                if (err) throw err;
+                client.close();
+                if(data)
+                res.json({
+                    message: "sucess",
+                    data:data
+                })
+                else{
+                    res.json({
+                        message: "failed"
+                    })
+                }
+            })
+            // Store hash in your password DB.
+        
+
+       // client.close();
+    });
+
+})
+
+app.post('/getanswersheet', function (req, res) {
+    console.log(req.body);
+    mongodbclient.connect(url, function (err, client) {
+        if (err) throw err;
+        var db = client.db(answersheetsDB);
+      
+           
+            
+                
+           
+            db.collection(answersheetscollection).findOne(req.body,function (err, data) {
+                console.log(data)
+                if (err) throw err;
+                client.close();
+                if(data)
+                res.json({
+                    message: "sucess",
+                    data:data
+                })
+                else{
+                    res.json({
+                        message: "failed"
+                    })
+                }
+            })
+            // Store hash in your password DB.
+        
+
+       // client.close();
+    });
+
+})
+
+
+
 app.post('/getquestionpaper', function (req, res) {
     console.log(req.body);
     mongodbclient.connect(url, function (err, client) {
@@ -367,156 +407,10 @@ app.post('/getquestionpaper', function (req, res) {
 })
 
 
-app.post('/displayproducts', function (req, res) {
-    console.log(req.body);
-    mongodbclient.connect(url, function (err, client) {
-        useUnifiedTopology: true
-        if (err) throw err;
-        var db = client.db(dbName);
-     
-            if(Object.keys(req.body).length === 0)
-            {
-                db.collection(dbCollection).find({}).toArray(function (err, data) {
-                    if (err) throw err;
-                    client.close();
-                    res.json({
-                        message: "saved",
-                        data:data
-                    })
-                })
-            }
-            else{
-            userdata = req.body
-           // db.collection(dbCollection).findOne(userData,{projection: {expense:true}}, function (err, data) {
-            db.collection(dbCollection).find(userdata).toArray(function (err, data) {
-                if (err) throw err;
-                client.close();
-                res.json({
-                    message: "saved",
-                    data:data
-                })
-            })
-        }
-            // Store hash in your password DB.
-        
 
-       // client.close();
-    });
-
-})
-
-app.post('/addproduct', function (req, res) {
-   
-    console.log(req.body);
-    mongodbclient.connect(url, function (err, client) {
-        if (err) throw err;
-        var db = client.db(dbName);
-        
-            
-          //  var userData = req.body
-                
-                
-            
-           db.collection(dbCollection).insertOne(req.body, function (err, data) {
-                if (err) throw err;
-                client.close();
-                res.json({
-                    message: "saved"
-                })
-            })
-            // Store hash in your password DB.
-     
-
-       // client.close();
-    });
-
-})
-
-app.post('/editproduct', function (req, res) {
-    console.log(req.body);
-    mongodbclient.connect(url, function (err, client) {
-        if (err) throw err;
-        var db = client.db(dbName);
-        
-            
-            var filter ={
-                productid : req.body.productid
-                
-            }
-            console.log(filter)
-          
-            db.collection(dbCollection).updateOne(filter,{$set:req.body}, function (err, data) {
-                if (err) throw err;
-                client.close();
-                res.json({
-                    message: "sucess"
-                })
-            })
-            // Store hash in your password DB.
-    
-
-       // client.close();
-    });
-
-})
-
-app.post('/verifyproduct', function (req, res) {
-    console.log(req.body);
-    mongodbclient.connect(url, function (err, client) {
-        if (err) throw err;
-        var db = client.db(dbName);
-      
-           
-            
-                
-           
-            db.collection(dbCollection).findOne(req.body,function (err, data) {
-                console.log(data)
-                if (err) throw err;
-                client.close();
-                if(data)
-                res.json({
-                    message: "sucess",
-                    data:data
-                })
-                else{
-                    res.json({
-                        message: "failed"
-                    })
-                }
-            })
-            // Store hash in your password DB.
-        
-
-       // client.close();
-    });
-
-})
 //process.env.PORT
-app.listen(4123, function () {
+app.listen(process.env.PORT, function () {
 
     console.log("listening on port 4123");
 });
 
-/*
- editProduct(data):Observable<any>{
-    console.log(data)
-    return this.http.post('http://localhost:4123/editproduct',data)
-  }
-
-  verifyProduct(data):Observable<any>{
-    console.log(data)
-    return this.http.post('http://localhost:4123/verifyproduct',data)
-  }
-  */
- /*
- 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://antonyrahul96:<password>@cluster0-nl7jd.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-*/
